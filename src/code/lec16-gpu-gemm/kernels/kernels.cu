e4ae94c4d9385fd24bc18da345635be3715413da
CSWater <yangcs@whu.edu.cn>
2023-05-29 23:54:45 +0800

    add shared blocking version

--- src/code/lec16-gpu-gemm/kernels/kernels.cu
+++ src/code/lec16-gpu-gemm/kernels/kernels.cu
@@ -1,4 +1,5 @@
 #include "../include/kernel_00.cuh"
+#include "../include/kernel_01.cuh"
 #include "../include/typename.h"
 
 void pe_dgemm_v0(int M, int N, int K, pe_f64 alpha, pe_f64* A, pe_f64* B, 
@@ -11,4 +12,12 @@ void pe_dgemm_v0(int M, int N, int K, pe_f64 alpha, pe_f64* A, pe_f64* B,
     cudaDeviceSynchronize();
 }
 
-
+void pe_dgemm_v1(int M, int N, int K, pe_f64 alpha, pe_f64* A, pe_f64* B, 
+                 pe_f64 beta, pe_f64* C) {
+    cudaDeviceSynchronize();
+    dim3 blockDim(32,32);
+    //ceil(M/32) ceil(N/32)
+    dim3 gridDim((M + 31) >> 5, (N + 31) >> 5);
+    pe_gemm_v1<pe_f64, 32, 32, 32><<<gridDim, blockDim>>>(M,N,K,alpha,A,B,beta,C);
+    cudaDeviceSynchronize();
+}
